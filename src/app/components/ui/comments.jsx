754615784b455d7729228fb347cb099064d75f42
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-01-27 00:34:04 +0300

    final-fast-company-after_PT_3__4

--- src/app/components/ui/comments.jsx
+++ src/app/components/ui/comments.jsx
@@ -20,9 +20,9 @@ const Comments = () => {
     const isLoading = useSelector(getCommentsLoadingStatus());
 
     const comments = useSelector(getComments());
-    const handleSubmit = ({ content }) => {
-        console.log("data_handleSubmit:", { content });
-        dispatch(createComment({ content: content, pageId: userId }));
+    const handleSubmit = (data) => {
+        console.log("data_handleSubmit:", data);
+        dispatch(createComment({ ...data, pageId: userId }));
     };
     const handleRemoveComment = (id) => {
         dispatch(removeComment(id));
